90b26d8ddec70a9b6b2abe1ff63dcf6cbff147a2
Antti Lehikoinen <a.lehikoinen97@gmail.com>
2025-11-03 07:34:49 +0200

    Uuden js tiedoston luonti, base canvas, canvas width ja height, img src, onload ja new image siirto uuteen js tiedostoon, location objektin iso päivitys, canvas.js päivitystä, uusien src polkujen lisäys

--- Canvas.js
+++ Canvas.js
@@ -1,115 +1,47 @@
-//Canvas pohjatiedot
-let body = document.width
-
-let canvas01 = document.getElementById("canvas01");
-let canvasWidth = canvas01.width;
-let canvasHeight = canvas01.height;
-let ctx = canvas01.getContext("2d");
-const img01 = new Image();
-const img02 = new Image();
-const imgMixed = new Image();
-const imgboss = new Image();
-const imgavain = new Image();
-const imgBackground = new Image();
-
-
-let testPois = 0;
+// Muuttujia
+let testPois = 0; //mahdollisesti voi poistaa?
 let testPois2 = 0;
 let useless;
 
-// 0----w
-let avainHallussa = false; //Jos true, avain piirretään näkyville ja jota tarvitaan ovelle, jos true, oven klikatessa
-//päästään erilaiseen Canvas.js tiedostoon (taso 2), tämän tiedoston voi kopioida, tosin joitain asioita muutettava
-//esim musiikki, kuvat, tausta, inventori esineet ja niiden sijoitukset. Mutta tämä vasta kun mixing_window tietyssä vaiheessa
-//Ja luultavasti mixing_window:ille omat JS tiedot, tai yksinkertaisesti erilaiset "else if" ehdot jonka mukaan
-//piirretään/lisätään erilaiset inventori esineet, ilman että tarvitaan 3 uutta JS tiedostoa
-
-//> mutta, kun sirrytään uuteen JS tiedostoon, html lienee sama, mutta siirrytään joko erilliseen JS tiedostoon,
-//tai erilliseen html tiedostoon, tästä vielä varmistettava chatgpt:ltä, mikä ideaalisempaa, mättää kaikki JS tiedostot
-//samaan html tiedostoon, vai oltava sekä eri/uusi html ja js tiedosto?
-
-let tasoNumero = 1; //kenties voi käyttää tämänkaltaista esineiden, inventorin loopatessa? Voisi
-//päivittää joka kerta kun ovesta klikataan ja avain on hallussa? Ehkä? arvona 2 kun 2. tasossa jne
-//if (avainHallussa === true)...tämän voisi lisätä jos oven koordinaattia klikataan,
-//tätä mukaa sitten muistettava että "locations" voisi myös päivittää, kentän omille esineille omat
-//ID tai tunnisteet, joiden avulla voi loopata huoletta kentän numerosta riippuen? Tosin vielä työn alla
+// Avain tilanne ja taosnumero jolla siirrytään tasosta seuraavaan // 0----w
+let avainHallussa = false; 
+let tasoNumero = 1; 
+let inNextLevel = false; //mustalle ruudulle, on "true" sen aikana kun ovesta klikataan, menee "false" kun refreshCanvasissa
 
 //Globaali tavaravarasto, missä esineet pidetään.
-let inventory = [0, 0, 0, 0, 0, 0]; //6 loppuversiossa, pitänee käyttää mixing_windowissa omaa?
+let inventory = [0, 0, 0, 0, 0, 0]; //[]
 
 // Lisätty: inventoryn vasemman reunan perus-x (vähentää kovakoodattuja +215 jne.)
 const inventoryBaseX = 90; // aiemmin käytettiin 215
 
 //9.10.2025 AL - Is mixingWindow open, sen slotit, mixing tulos¨
-let isMixingWindowOpen = false; //tällä voi säätää kaiken muun pimentämistä ja klikattavuutta
+let isMixingWindowOpen = false; //tällä voi säätää kaiken muun piilottamista ja klikattavuutta
 
-
-
-//Location data
-const locations = {
-    Img1: {
-        x: (canvasWidth/2 - 120),
-        y: 150,
-        width: 230,
-        height: 230 
-    },
-
-    // Image 02 location data (esim. oikealle item_01:stä)
-    Img2: {
-        x: (canvasWidth/2 + 120),
-        y: 150,
-        width: 230,
-        height: 230
-    },
-
-    boss: {
-        x: (canvasWidth/2 - 150),
-        y: 20,
-        width: 300,
-        height: 200,
-        alive: true
-    },
-
-    background: {
-        x: 0,
-        y: 0,
-        width: canvasWidth,
-        height: canvasHeight
-    },
-
-
-};
-
-imgBackground.onload = function() {
-    refreshCanvas();
-}
-
-img01.onload = function() {
-    refreshCanvas();
-}
-img02.onload = function() {
-    refreshCanvas();
-}
-imgMixed.src = "kuvat/Mixed_01.png";
-
-imgboss.onload = function() {
-    refreshCanvas();
-}
-
-imgavain.onload = function() {
-    refreshCanvas();
-}
+// Tällä new Image(), canvasin pohja, location, img src, img onload
+genImageOnload();
 
 function refreshCanvas() {
-    //resetoi canvas
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
-    //Canvas tausta
-    ctx.drawImage(imgBackground, 0, 0, canvasWidth, canvasHeight);
+
+    //resetoi canvas ja canvas:in tausta
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    //poistu mustasta ruudusta
+    if (inNextLevel === true) {
+        ctx.fillStyle = "rgba(0,0,0,0.8)";
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        
+        setTimeout(() => {
+            inNextLevel = false;
+            refreshCanvas()
+        }, 2000);
+    }
+
+    const stageNow = locations.stages[tasoNumero - 1];
+
+    ctx.drawImage(stageNow.background.value, 0, 0, canvasWidth, canvasHeight);
 
 
-    //9.10.2025 AL, mixing_window.js testi
-    //Luo uuden mixaus buttonin, ja jos siitä klikataan canvas01.addEventListener:in
-    //sisällä, kutsuu se funktiota, mikä Itsessään sijaitsee mixing_window.js tiedostossa, missä se window piirretään
+
     if (isMixingWindowOpen === false){
        
         ctx.fillStyle = "#65649fff";
@@ -134,16 +66,18 @@ function refreshCanvas() {
     }
 
     // Piirrä item_01, jos sitä ei ole otettu
-    if (testPois === 0){
-        ctx.drawImage(img01, locations.Img1.x, locations.Img1.y, locations.Img1.width, locations.Img1.height);
+    // Tämän voisi koodata uudelleen erilaisella tavalla, mutta saa kelvata toistaiseksi
+    // Voi toimia 1. kentässä mutta ei jatkossa
+    if (stageNow.images[0].obtained !== 1){
+        ctx.drawImage(stageNow.images[0].value, stageNow.images[0].x, stageNow.images[0].y, stageNow.images[0].width, stageNow.images[0].height);
     }
     // Piirrä item_02, jos sitä ei ole otettu
-    if (testPois2 === 0){
-        ctx.drawImage(img02, locations.Img2.x, locations.Img2.y, locations.Img2.width, locations.Img2.height);
+    if (stageNow.images[1].obtained !== 1){
+        ctx.drawImage(stageNow.images[1].value, stageNow.images[1].x, stageNow.images[1].y, stageNow.images[1].width, stageNow.images[1].height);
     }
     // Piirrä boss
-    if (locations.boss.alive) {
-        ctx.drawImage(imgboss, locations.boss.x, locations.boss.y, locations.boss.width, locations.boss.height);
+    if (stageNow.boss.alive) { //`item_${item_number}`)
+        ctx.drawImage(stageNow.boss.value, stageNow.boss.x, stageNow.boss.y, stageNow.boss.width, stageNow.boss.height);
     }
 
 
@@ -152,17 +86,23 @@ function refreshCanvas() {
         ctx.drawImage(imgavain, 90, 529, 95, 95);
     }
 
-
     // Piirrä inventaarion esineet
+    // Tätä osiota kenties muokattava, jos tarkoitus edetä kentästä toiseen, ehdottomasti
     let invIndex = 0;
     for (let i = 0; i < inventory.length; i++) {
         let item = inventory[i];
        
         if (item === "Item_01") {
-            ctx.drawImage(img01, (invIndex * 110) + inventoryBaseX, 615, 120, 120);
+            ctx.drawImage(stageNow.images[0].value, (invIndex * 110) + inventoryBaseX, 615, 120, 120);
             invIndex++;
         } else if (item === "Item_02") {
-            ctx.drawImage(img02, (invIndex * 110) + inventoryBaseX, 615, 120, 120);
+            ctx.drawImage(stageNow.images[1].value, (invIndex * 110) + inventoryBaseX, 615, 120, 120);
+            invIndex++;
+        } else if (item === "Item_03") {
+            ctx.drawImage(stageNow.images[0].value, (invIndex * 110) + inventoryBaseX, 615, 120, 120);
+            invIndex++;
+        } else if (item === "Item_04") {
+            ctx.drawImage(stageNow.images[1].value, (invIndex * 110) + inventoryBaseX, 615, 120, 120);
             invIndex++;
         } else if (item === "MixTest") {
             ctx.drawImage(imgMixed, (invIndex * 110) + inventoryBaseX, 615, 120, 120);
@@ -172,31 +112,11 @@ function refreshCanvas() {
         // Lisää muita esineitä tarvittaessa
     }
 
-    // Piirrä yhdistys-nappi canvasin päälle
-    //drawMixButton();
 } 
-
-
-
-// Poista vanhat click-listenerit (ettei tule tuplaklikkauksia) //28.10.25 kommentoin tämän jotta tämänhetkinen koodi toimisi
-//canvas01.replaceWith(canvas01.cloneNode(true));
-//canvas01 = document.getElementById("canvas01");
-//ctx = canvas01.getContext("2d");
 
 canvas01.addEventListener("click", (e) => {
     const mouseX = e.offsetX;
     const mouseY = e.offsetY;
-
-    //************************************************
-    // Ehdotukset chatgpt:ltä, varmuuden vuoksi jos jotain hyötyä, muussa tapauksessa voi poistaa
-
-    //const rect = canvas01.getBoundingClientRect();
-    //const scaleX = canvas01.width / rect.width;
-    //const scaleY = canvas01.height / rect.height;
-
-    //const mouseX = (e.clientX - rect.left) * scaleX;
-    //const mouseY = (e.clientY - rect.top) * scaleY;
-    //************************************************
 
     //AL 10.10.25 - Jos Mixaus painikkeesta painetaan inventorin vieressä, siirrytään Mixaus ikkunaan
     //1340, 610, 130, 120);
@@ -227,66 +147,65 @@ function handleFirstScreen(x, y){
         return;
     }
 
-    // item_01
-    if (
-        testPois === 0 &&
-        x >= locations.Img1.x &&
-        x <= locations.Img1.x + locations.Img1.width &&
-        y >= locations.Img1.y &&
-        y <= locations.Img1.y + locations.Img1.height
-    ){
-        if (isMixingWindowOpen === false){
-            testPois = 1;
-            inventory.unshift("Item_01");
+    // Kenttä rippuen tasonumerosta
+    const stageNow = locations.stages[tasoNumero - 1];
+    
+    //Item tarkistus
+    for (let i = 0; i < stageNow.images.length; i++) {
+        const item = stageNow.images[i];
+        if (
+            item.obtained === 0 &&
+            x >= item.x && x <= item.x + item.width &&
+            y >= item.y && y <= item.y + item.height
+        ) {
+            inventory.unshift(item.name);
             useless = inventory.pop();
+            item.obtained = 1;
             refreshCanvas();
             return;
         }
-
     }
-
-    // item_02
-    if (
-        testPois2 === 0 &&
-        x >= locations.Img2.x &&
-        x <= locations.Img2.x + locations.Img2.width &&
-        y >= locations.Img2.y &&
-        y <= locations.Img2.y + locations.Img2.height
-    ){
-        if (isMixingWindowOpen === false){
-            testPois2 = 1;
-            inventory.unshift("Item_02");
-            useless = inventory.pop();
-            refreshCanvas();
-            return;
-        }
-
-    }
+    //('item_${item_number}') esim, kun tarkoitus pitää kirjaa missä kentässä mennään?
 
     // Boss Poisto test
+    // Check boss clicks (if applicable)
+    const boss1 = locations.stages[tasoNumero - 1].boss;
     if (
-        x >= locations.boss.x &&
-        x <= locations.boss.x + locations.boss.width &&
-        y >= locations.boss.y &&
-        y <= locations.boss.y + locations.boss.height
-        && inventory.includes("MixTest")
+        boss1.alive === true &&
+        x >= boss1.x && x <= boss1.x + boss1.width &&
+        y >= boss1.y && y <= boss1.y + boss1.height
     ){
         
-        if (isMixingWindowOpen === false){
+        if (isMixingWindowOpen === false && inventory.includes("MixTest")){
             inventory = inventory.filter(item => item !== "MixTest");
-            locations.boss.alive = false;
+            boss1.alive = false;
             console.log("Bossi on voitettu!");
             inventory.unshift(0);
+            //useless = inventory.pop();
             //AL 25.10.25. Tässä todennäköisesti aktivoidaan muttujan arvo, mikä lisää avaimen käyttöön
             //Ja tällä avaimella, saadaan muutokset aikaan kun ovesta painetaan, mikä vie meidät erilaiseen tasoon
-            avainHallussa = true;
-        
+            avainHallussa = true;        
             refreshCanvas();
             return;
-        }
+        }}
+        //else if jos erilainen tulos inventorissa, tosin tämä ei varmaan paras mahdollinen tapa
+        //else if()){
 
-    }
-} 
+        //}
+
+    //oven klikkaus, josta tasosta siirtyminen toiseen?
+    if (
+        avainHallussa === true &&
+        x >= stageNow.door.x && x <= stageNow.door.x + stageNow.door.width &&
+        y >= stageNow.door.y && y <= stageNow.door.y + stageNow.door.height
+    ){
+        inNextLevel = true;
+        tasoNumero++;
+        inventory = [0, 0, 0, 0, 0, 0];
+        avainHallussa = false;
+        refreshCanvas();
+        }
+    } 
 
 function handleMixingScreen(x, y){
     console.log(x, y);
@@ -357,9 +276,3 @@ function handleMixingScreen(x, y){
         }
 
 }
-
-img01.src = "kuvat/Item_01.png";
-img02.src = "kuvat/Item_02.png";
-imgboss.src = "kuvat/taso_1_kuvat/Boss_01.png";
-imgavain.src = "kuvat/key_item.png";
-imgBackground.src = "kuvat/taso_1_kuvat/Taso_1.png";
---
+++ image_canvas.js
@@ -0,0 +1,194 @@
+//new Image() data
+//module vaihtoehto - const images = {img01: new Image(),}
+//const images = {kentta1: {img01: new Image(), img02 = new Image(),....}}
+const img01 = new Image();
+const img02 = new Image();
+const img03 = new Image();
+const img04 = new Image();
+const imgMixed = new Image();
+const imgboss1 = new Image();
+const imgboss2 = new Image();
+const imgavain = new Image();
+const imgBackground = new Image();
+const imgBackground2 = new Image();
+const imgBackground3 = new Image();
+const imgBackground4 = new Image();
+/* const imgboss2 = new Image();
+const img03 //kenttä 2
+const img04 = new Image();
+const img05 = new Image();
+*/
+//Canvas info, Canvas pohjatiedot
+let body = document.width
+let canvas01 = document.getElementById("canvas01");
+let ctx = canvas01.getContext("2d");
+let canvasWidth = canvas01.width;
+let canvasHeight = canvas01.height
+
+//Location data
+/*
+const locations = {
+    Img1: {
+        x: (canvasWidth/2 - 120),
+        y: 150,
+        width: 230,
+        height: 230 
+    },
+
+    // Image 02 location data (esim. oikealle item_01:stä)
+    Img2: {
+        x: (canvasWidth/2 + 120),
+        y: 150,
+        width: 230,
+        height: 230
+    },
+
+    boss: {
+        x: (canvasWidth/2 - 150),
+        y: 20,
+        width: 300,
+        height: 200,
+        alive: true
+    },
+
+    background: {
+        x: 0,
+        y: 0,
+        width: canvasWidth,
+        height: canvasHeight 
+    },
+};
+*/
+
+/*const soundEffects = {
+ stages: [
+       { // All_Stages
+          item_click: value: item_click.src,
+          button_click: value: button_click.src,
+          boss_click_fail: value: boss_click_1.src,
+          boss_click_success: value: boss_click_2.src,
+          door_click: value: door_click.src,
+          mix_fail: value: mix_fail.src,
+          mix_success: mix_success.src,
+          game_over: game_over.src,
+          hint_click: hint_click.src,
+          minighost_click: minighost_click.src
+          }
+        ]
+    }
+*/
+
+//1. tasosta 2. tasoon sen mukaan minkä arvoinen kenttänumero muuttuja on, numeron päivitettyä funktio uudelleen ladataan?
+//Lisää sitten tarkemmat koordinaatit muokatuista tiedostoista mitkä jäsen päivittänyt
+const locations = {
+    stages: [
+        { // Stage 1
+            id: 1,
+            background: { x: 0, y: 0, width: canvasWidth, height: canvasHeight, value: imgBackground},
+            music: "tba",
+            boss: { x: 500, y: 250, width: 250, height: 250, value: imgboss1, alive: true},
+            door: { x: 450, y: 300, width: 170, height: 160, value: "Door_01" },
+            images: [
+                { x: (canvasWidth/2 - 120), y: 150, width: 150, height: 150, value: img01, name: "Item_01", obtained: 0},
+                { x: (canvasWidth/2 + 220), y: 120, width: 150, height: 150, value: img02, name: "Item_02", obtained: 0}
+            ]
+        },
+        { // Stage 2
+            id: 2,
+            background: { x: 0, y: 0, width: canvasWidth, height: canvasHeight, value: imgBackground2},
+            music: "tba",
+            boss: { x: 500, y: 250, width: 250, height: 250, value: imgboss2, alive: true},
+            door: { x: 800, y: 400, width: 120, height: 200, value: "Door_02" },
+            images: [
+                { x: 150, y: 250, width: 220, height: 180, value: img03, name: "Item_03", obtained: 0},
+                { x: 350, y: 450, width: 310, height: 180, value: img04, name: "Item_04", obtained: 0}
+            ]
+        },
+        { // Stage 3
+            id: 3,
+            background: { x: 0, y: 0, width: canvasWidth, height: canvasHeight, value: imgBackground3.src},
+            music: "tba",
+            boss: { x: 500, y: 250, width: 250, height: 250, value: "Boss_03", alive: true},
+            door: { x: 800, y: 400, width: 120, height: 200, value: "Door_03" },
+            images: [
+                { x: 100, y: 200, width: 230, height: 190, value: "Item_05", obtained: 0},
+                { x: 420, y: 470, width: 320, height: 200, value: "Item_06", obtained: 0}
+            ]
+        },
+        { // Stage 4
+            id: 4,
+            background: { x: 0, y: 0, width: canvasWidth, height: canvasHeight, value: imgBackground4.src},
+            music: "tba",
+            boss: { x: 500, y: 250, width: 250, height: 250, value: "Boss_04", alive: true},
+            door: { x: 800, y: 400, width: 120, height: 200, value: "Door_04" },
+            images: [
+                { x: 100, y: 200, width: 230, height: 190, value: "Item_07", obtained: false},
+                { x: 420, y: 470, width: 320, height: 200, value: "Item_08", obtained: false }
+            ]
+        }
+    ]
+};
+
+
+//alt - setImageSource(){images.img01.src = "kuvat-Item_01.png",...} then call this function on canvas
+//taso 1 - taso 4 ... images.taso1.img01.src =kuvat-item01.png" 
+img01.src = "kuvat/Item_01.png";
+img02.src = "kuvat/Item_02.png";
+img03.src = "kuvat/taso_2_kuvat/Lukittu_boxi.png";
+img04.src = "kuvat/taso_2_kuvat/Lukittu_boxi_alt.png";
+imgboss1.src = "kuvat/taso_1_kuvat/Boss_01.png";
+imgboss2.src = "kuvat/taso_2_kuvat/Boss_02.png";
+imgMixed.src = "kuvat/Mixed_01.png";
+imgavain.src = "kuvat/key_item.png";
+imgBackground.src = "kuvat/taso_1_kuvat/Taso_1.png";
+imgBackground2.src = "kuvat/taso_2_kuvat/Taso_2.png";
+imgBackground3.src = "kuvat/taso_3_kuvat/Taso_3.png";
+imgBackground4.src = "kuvat/taso_4_kuvat/Taso_4.png";
+
+//image onload data
+//alt - setImageOnload(){images.img01.onload = refreshCanvas,...} then call this function on canvas
+function genImageOnload(){
+    
+    document.addEventListener("DOMContentLoaded", function() {
+        
+        imgBackground.onload = function() {
+        refreshCanvas();
+    }    
+        imgBackground2.onload = function() {
+        refreshCanvas();
+    }  
+        img01.onload = function() {
+        refreshCanvas();
+    }    
+        img02.onload = function() {
+        refreshCanvas();
+    }
+        img03.onload = function() {
+        refreshCanvas();
+    }  
+        img04.onload = function() {
+        refreshCanvas();
+    }    
+        imgboss1.onload = function() {
+        refreshCanvas();
+    }  
+        imgboss2.onload = function() {
+        refreshCanvas();
+    }    
+        imgavain.onload = function() {
+        refreshCanvas();
+    }
+}
+)};
+
+/*for (const key in img) {
+  img[key].onload = function() {
+    refreshCanvas();
+  };
+}
+*/
+
+//Vaihtoehto jos tarvetta
+//export {images, locations, genImageOnLoad};
+
+//loadImages() //or genImageOnload()
